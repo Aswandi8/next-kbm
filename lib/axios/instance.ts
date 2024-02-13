fdcf5449094943fe98cc3e67fdb537d1dd93c96e
Aswandi8 <aswandibaso08@gmail.com>
2024-02-14 02:31:19 +0800

    First Commit 2

--- lib/axios/instance.ts
+++ lib/axios/instance.ts
@@ -8,7 +8,7 @@ const headers = {
 };
 
 const instance = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers,
   timeout: 60 * 1000,
 });
