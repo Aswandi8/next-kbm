c4abafa4205d1e68dc5161e20e058f3301d5aac6
achmadbntngg <achmadbintang501@gmail.com>
2024-02-21 13:58:37 +0800

    Features: Navigation

---
+++ app/(root)/keuangan/account/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import MyButton from "@/app/components/ui/button";
+import { signOut } from "next-auth/react";
+
+const AccountKeuangan = () => {
+  return (
+    <div>
+      <MyButton
+        text="Logout"
+        customFunc={() => signOut({ callbackUrl: "/auth/sign-in" })}
+      />
+      <div>
+        <h1>sdfhsdf</h1>
+      </div>
+      <h1>Home Keuangan</h1>
+    </div>
+  );
+};
+export default AccountKeuangan;
